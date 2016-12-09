624cd41d4750fdb95325b57e85286fd723966dc6
Daniel Blanco <daniel@blancomartin.cl>
2016-12-08 22:45:55 -0300

    [FIX] Syntax correction

--- global_discount/models/account_invoice.py
+++ global_discount/models/account_invoice.py
@@ -13,7 +13,9 @@ class DiscBase(models.Model):
         for tax in self:
             base = 0.0
             if tax.invoice_id.global_discount_type in ['percent']:
-                tax.base = round(tax.base * (1 - ((tax.invoice_id.global_discount / 100.0) or 0.0)))
+                tax.base = round(
+                    tax.base * (
+                        1 - ((tax.invoice_id.global_discount / 100.0) or 0.0)))
             else:
                 tax.base -= tax.invoice_id.global_discount
 
@@ -21,11 +23,21 @@ class DiscBase(models.Model):
 class GlobalDiscount(models.Model):
     _inherit = "account.invoice"
 
-    #global_discount = fields.Many2one('discounts', string="Seleccione Descuento Global")
-    global_discount = fields.Float(string="Descuento Global", default=0.00, readonly=True, states={'draft': [('readonly', False)]})
-    global_discount_type = fields.Selection([('amount','Monto'),('percent','Porcentaje')], string="Tipo de descuento", readonly=True, states={'draft': [('readonly', False)]})
-    global_discount_detail = fields.Char(string="Razón del descuento", readonly=True, states={'draft': [('readonly', False)]})
-    amount_untaxed_global_discount = fields.Float(string="Global Discount Amount", store=True, default=0.00, compute='_compute_amount')
+    # global_discount = fields.Many2one(
+    # 'discounts', string="Seleccione Descuento Global")
+    global_discount = fields.Float(
+        string="Descuento Global", default=0.00, readonly=True,
+        states={'draft': [('readonly', False)]})
+    global_discount_type = fields.Selection(
+        [('amount', 'Monto'), ('percent', 'Porcentaje')],
+        string="Tipo de descuento", readonly=True,
+        states={'draft': [('readonly', False)]})
+    global_discount_detail = fields.Char(
+        string="Razón del descuento", readonly=True,
+        states={'draft': [('readonly', False)]})
+    amount_untaxed_global_discount = fields.Float(
+        string="Global Discount Amount", store=True, default=0.00,
+        compute='_compute_amount')
 
     @api.multi
     def get_taxes_values(self):
@@ -45,7 +57,8 @@ class GlobalDiscount(models.Model):
             for id, t in taxes.iteritems():
                 tax = self.env['account.tax'].browse(t['tax_id'])
                 if tax.amount > 0 and discount > 0:
-                    t['amount'] = (t['amount'] * (1 - ((discount / 100.0) or 0.0)))
+                    t['amount'] = (
+                        t['amount'] * (1 - ((discount / 100.0) or 0.0)))
                 tax_grouped[id] = t
         return tax_grouped
 
@@ -59,7 +72,8 @@ class GlobalDiscount(models.Model):
                     tax_lines += tax_lines.new(tax)
                 inv.tax_line_ids = tax_lines
                 discount = inv.global_discount
-                amount_untaxed = round(sum(line.price_subtotal for line in inv.invoice_line_ids))
+                amount_untaxed = round(
+                    sum(line.price_subtotal for line in inv.invoice_line_ids))
                 afecto = 0
                 for line in inv.invoice_line_ids:
                     for t in line.invoice_line_tax_ids:
@@ -69,19 +83,25 @@ class GlobalDiscount(models.Model):
                     discount = ((inv.global_discount * 100) / afecto )
                 inv.amount_untaxed = amount_untaxed
                 if afecto > 0 and discount > 0:
-                    inv.amount_untaxed_global_discount = (afecto * (discount / 100))
-                amount_untaxed -= round(inv.amount_untaxed_global_discount)
+                    inv.amount_untaxed_global_discount = (
+                        afecto * (discount / 100))
+                amount_untaxed -= round(
+                    inv.amount_untaxed_global_discount)
                 amount_tax = sum(line.amount for line in inv.tax_line_ids)
                 inv.amount_tax = amount_tax
                 amount_total = amount_untaxed + amount_tax
                 amount_total_company_signed = amount_total
                 inv.amount_total = amount_total
                 amount_untaxed_signed = amount_untaxed
-                if inv.currency_id and inv.currency_id != inv.company_id.currency_id:
-                    amount_total_company_signed = inv.currency_id.compute(amount_total, inv.company_id.currency_id)
-                    amount_untaxed_signed = inv.currency_id.compute(amount_untaxed, inv.company_id.currency_id)
+                if inv.currency_id and inv.currency_id != inv.company_id.\
+                        currency_id:
+                    amount_total_company_signed = inv.currency_id.compute(
+                        amount_total, inv.company_id.currency_id)
+                    amount_untaxed_signed = inv.currency_id.compute(
+                        amount_untaxed, inv.company_id.currency_id)
                 sign = inv.type in ['in_refund', 'out_refund'] and -1 or 1
-                inv.amount_total_company_signed = amount_total_company_signed * sign
+                inv.amount_total_company_signed = amount_total_company_signed *\
+                                                  sign
                 inv.amount_total_signed = amount_total * sign
                 inv.amount_untaxed_signed = amount_untaxed_signed * sign
             else:
@@ -89,7 +109,8 @@ class GlobalDiscount(models.Model):
 
     def finalize_invoice_move_lines(self, move_lines):
         if not self.global_discount > 0:
-            return super(GlobalDiscount,self).finalize_invoice_move_lines( move_lines)
+            return super(GlobalDiscount, self).finalize_invoice_move_lines(
+                move_lines)
         new_lines = []
         discount = self.global_discount
         if self.global_discount_type == 'amount':
@@ -102,15 +123,18 @@ class GlobalDiscount(models.Model):
         hold = False
         total = 0
         for line in move_lines:
-            if line[2]['tax_ids'] and not line[2]['tax_line_id']:#iva ya viene con descuento
+            if line[2]['tax_ids'] and not line[2]['tax_line_id']:
+                #iva ya viene con descuento
                 if line[2]['tax_ids']:
                     for t in line[2]['tax_ids']:
                         imp = self.env['account.tax'].browse(t[1])
                         if imp.amount > 0  and line[2]['debit'] > 0:
-                            line[2]['debit'] -= int(round((line[2]['debit'] * (discount / 100))))
+                            line[2]['debit'] -= int(
+                                round((line[2]['debit'] * (discount / 100))))
                             total += line[2]['debit']
                         elif imp.amount > 0:
-                            line[2]['credit'] -= int(round((line[2]['credit'] * (discount /100))))
+                            line[2]['credit'] -= int(
+                                round((line[2]['credit'] * (discount /100))))
                             total += line[2]['credit']
                         elif line[2]['debit'] > 0:
                             total += line[2]['debit']
@@ -131,16 +155,16 @@ class GlobalDiscount(models.Model):
             hold[2]['credit'] = total
         new_lines.extend([hold])
 
-        new_lines = super(GlobalDiscount,self).finalize_invoice_move_lines( new_lines)
+        new_lines = super(GlobalDiscount, self).finalize_invoice_move_lines(
+            new_lines)
         return new_lines
 
     def _dte(self, n_atencion=None):
-        dte = super(GlobalDiscount,self)._dte(n_atencion)
+        dte = super(GlobalDiscount, self)._dte(n_atencion)
         if not self.global_discount > 0:
             return dte
         result = collections.OrderedDict()
         lin_dr = 1
-        dr_line = {}
         dr_line = collections.OrderedDict()
         dr_line['NroLinDR'] = lin_dr
         dr_line['TpoMov'] = 'D'
@@ -150,8 +174,11 @@ class GlobalDiscount(models.Model):
         if self.global_discount_type == "amount":
             disc_type = "$"
         dr_line['TpoValor'] = disc_type
-        dr_line['ValorDR'] = round(self.global_discount,2)
-        if self.sii_document_class_id.sii_code in [34] and (self.referencias and self.referencias[0].sii_referencia_TpoDocRef.sii_code == '34'):#solamente si es exento
+        dr_line['ValorDR'] = round(self.global_discount, 2)
+        if self.sii_document_class_id.sii_code in [34] and (
+                    self.referencias and self.referencias[0].
+                        sii_referencia_TpoDocRef.sii_code == '34'):
+            # solamente si es exento
             dr_line['IndExeDR'] = 1
         dr_lines= [{'DscRcgGlobal':dr_line}]
         for key, value in dte.iteritems():
@@ -162,5 +189,5 @@ class GlobalDiscount(models.Model):
 
     def _dte_to_xml(self, dte, tpo_dte="Documento"):
         xml = super(GlobalDiscount, self)._dte_to_xml(dte, tpo_dte)
-        xml = xml.replace('<drlines>','').replace('</drlines>','')
+        xml = xml.replace('<drlines>', '').replace('</drlines>', '')
         return xml
