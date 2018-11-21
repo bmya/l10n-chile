f21fade6f8e325fad192e8a48fac466bdde4df75
Daniel Blanco <daniel@bmya.cl>
2018-11-21 01:19:26 -0300

    [FIX] change name instead of id (without module)

--- l10n_cl_account_journal_reports/wizard/account_journal_wizard.py
+++ l10n_cl_account_journal_reports/wizard/account_journal_wizard.py
@@ -108,7 +108,7 @@ class AccountJournalBookWizard(models.TransientModel):
         #     'l10n_cl_account_journal_reports.account_journal_book_xlsx').report_action(records, data=data)
         # l10n_cl_account_journal_reports.account_journal_book_xlsx
         return self.env['report'].get_action(
-            records, 'l10n_cl_account_journal_reports.account_journal_book_xlsx', data=data)
+            records, 'journal_xlsx', data=data)
 
 
     @api.multi
